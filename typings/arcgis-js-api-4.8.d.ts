6a69f840428d323de78cadab248560c689d85b5f
JC Franco <jfranco@esri.com>
2018-06-26 07:35:15 -0700

    Add custom bookmarks demo.

--- typings/arcgis-js-api-4.8.d.ts
+++ typings/arcgis-js-api-4.8.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for ArcGIS API for JavaScript 4.8
 // Project: http://js.arcgis.com
 // Definitions by: Esri <https://github.com/Esri>
-//                 Bjorn Svensson <https://github.com/bsvensson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
